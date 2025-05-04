387641c083689c1e240554594b11327e710dfea7
ThuyPham <92603796+ThuyPhamict@users.noreply.github.com>
2025-05-04 13:40:10 -0500

    fix bug

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -7,13 +7,13 @@ require('dotenv').config();
 router.post('/', async(req,res) => {
     try{
         const { orderId, productId, quantity, unit_price } = req.body;
-        
+        console.log(orderId);
 
         const orderItemsResult = await db.query(
             `INSERT INTO orderitems(order_id, product_id, quantity, unit_price) VALUES ($1,$2,$3,$4) RETURNING *`,
             [orderId, productId, quantity, unit_price ]
         );
-
+        
         res.status(201).json({ success: true, order: orderItemsResult.rows[0] });
     }catch (err) {
         console.error("Error creating order:", err);
