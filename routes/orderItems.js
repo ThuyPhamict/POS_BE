5424baa6fb12553b953a27b293da8ad7d2614f71
ThuyPham <92603796+ThuyPhamict@users.noreply.github.com>
2025-05-04 13:55:57 -0500

    orderItems API

--- routes/orderItems.js
+++ routes/orderItems.js
@@ -6,13 +6,22 @@ require('dotenv').config();
 // Update orders 
 router.post('/', async(req,res) => {
     try{
-        const { orderId, productId, quantity, unit_price } = req.body;
-        console.log(orderId);
+        // const { orderId, productId, quantity, unit_price } = req.body;
 
-        const orderItemsResult = await db.query(
-            `INSERT INTO orderitems(order_id, product_id, quantity, unit_price) VALUES ($1,$2,$3,$4) RETURNING *`,
-            [orderId, productId, quantity, unit_price ]
-        );
+        // const orderItemsResult = await db.query(
+        //     `INSERT INTO orderitems(order_id, product_id, quantity, unit_price) VALUES ($1,$2,$3,$4) RETURNING *`,
+        //     [orderId, productId, quantity, unit_price ]
+        // );
+
+        const { orderId, items} = req.body;
+
+        items.array.forEach(element => {
+            const { productId, quantity, unit_price} = element; 
+            const orderItemsResult = db.query(
+                `INSERT INTO orderitems(order_id, product_id, quantity, unit_price) VALUES ($1,$2,$3,$4) RETURNING *`,
+                [orderId, productId, quantity, unit_price ]
+            );
+        });
         
         res.status(201).json({ success: true, order: orderItemsResult.rows[0] });
     }catch (err) {
